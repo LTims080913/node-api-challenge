05398ba2cf4ff9418c0cfe180526123087bba168
Latosha Tims <ltims080913@hotmail.com>
2020-05-08 10:40:52 -0700

    finished actionRouter. functionality complete. MVP met.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.get('/', (req, res) => {
 })
 
 server.use('/api/projects', projectRouter)
-// server.use('/api/actions', actionRouter)
+server.use('/api/actions', actionRouter)
 
 function logger(req, res, next) {
     console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`);
